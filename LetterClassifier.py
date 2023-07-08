8962667d6d85f83fd2fcf21105a6b610a9f26ffe
BuildTools <unconfigured@null.spigotmc.org>
2023-07-08 23:10:32 +0200

    training data

--- LetterClassifier.py
+++ LetterClassifier.py
@@ -2,7 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-#Monochrome, 32x32 bitmap, pen tool 3px thickness, a-z lower case, 26 letters
+#Monochrome, 32x32 bitmap, pen tool 3px thickness, a-z lower case, 26 letters abcdef ghijklmnop qrstuvxyz
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 class LetterNet(nn.Module):
     def __init__(self):
