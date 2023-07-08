70fda853d613131787650f77173c4bac936d796c
BuildTools <unconfigured@null.spigotmc.org>
2023-07-08 21:54:37 +0200

    started letter classifier

---
+++ LetterClassifier.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+#Monochrome, 32x32 bitmap, pen tool 3px thickness, a-z lower case, 26 letters
+
+class LetterNet(nn.Module):
+    def __init__(self):
+        super(LetterNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 3, 5)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        return x
+
+net = LetterNet()
+
+print(net)
