0e39b4772715bd00ff8e17e6fd0afd7ae3491b77
BuildTools <unconfigured@null.spigotmc.org>
2023-07-09 22:13:08 +0200

    work on dataset

--- LetterClassifier.py
+++ LetterClassifier.py
@@ -2,10 +2,20 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import os
+import random
+from PIL import Image
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 #Monochrome, 32x32 bitmap, pen tool 3px thickness, a-z lower case, 26 letters abcdef ghijklmnop qrstuvxyz
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+data_path = Path("data/letter_classification/")
+train_dir = data_path / "train"
+test_dir = data_path / "test"
 
 class LetterNet(nn.Module):
     def __init__(self):
@@ -18,4 +28,33 @@ class LetterNet(nn.Module):
 
 net = LetterNet()
 
-print(net)
+random.seed(32)
+
+data_path_list = list(data_path.glob("*/*/*.jpg"))
+
+random_image_path = random.choice(data_path_list)
+
+image_class = random_image_path.parent.stem
+
+data_transform = transforms.Compose([
+    #transforms.TrivialAugmentWide(),
+    transforms.ToTensor()
+    ])
+
+training_data = datasets.ImageFolder(root=train_dir,
+                                          transform=data_transform,
+                                          target_transform=None)
+
+#testing_data = datasets.ImageFolder(root=test_dir,
+#                                    transform=data_transform)
+#Not made yet :)
+
+training_dataloader = DataLoader(dataset=training_data,
+                                 batch_size=1,
+                                 num_workers=0,
+                                 shuffle=True)
+
+img, label = next(iter(training_dataloader))
+
+print(f"Image shape: {img.shape} -> [batch_size, color_channels, height, width]")
+print(f"Label shape: {label.shape}")
