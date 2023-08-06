09a22f18cdbef4387d3dedda6710e47d96cf11d1
BuildTools <unconfigured@null.spigotmc.org>
2023-08-06 18:21:54 +0200

    Using EMNIST dataset because my own letters were too few. Doesn't compile atm

--- PyTorch_NN.py
+++ PyTorch_NN.py
@@ -12,13 +12,13 @@ from torchvision import datasets, transforms
 import torch.optim as optim
 import datetime, time
 
-#Monochrome, 32x32 bitmap, pen tool 3px thickness, a-z lower case, 26 letters abcdef ghijklmnop qrstuvxyz
+#Monochrome, 28x28 bitmap, pen tool 3px thickness, a-z lower case, 26 letters abcdef ghijklmnop qrstuvxyz
 
 #Coarse parameters
-train=True
-epochs=25
+train=False
+epochs=1
 
-device = 'cpu'#'cuda' if torch.cuda.is_available() else 'cpu' TODO: Something with assertion while using cuda breaks, compile with TORCH_USE_CUDA_DSA suggestion
+device = 'cuda' if torch.cuda.is_available() else 'cpu' #TODO: Something with assertion while using cuda breaks, compile with TORCH_USE_CUDA_DSA suggestion
 
 data_path = Path("data/letter_classification/")
 train_dir = data_path / "train"
@@ -27,11 +27,11 @@ test_dir = data_path / "test"
 class LetterNet(nn.Module):
     def __init__(self):
         super(LetterNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 25, 5)
+        self.conv1 = nn.Conv2d(3, 50, 28)
         self.pool = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(25,35,5)
-        self.fc1 = nn.Linear(35*5*5,120)
-        self.fc2 = nn.Linear(120, 84)
+        self.conv2 = nn.Conv2d(50,60,5)
+        self.fc1 = nn.Linear(60*5*5,200)
+        self.fc2 = nn.Linear(200, 84)
         self.fc3 = nn.Linear(84, 26)
 
     def forward(self, x):
@@ -51,22 +51,35 @@ data_transform = transforms.Compose([
     transforms.ToTensor()
     ])
 
-training_data = datasets.ImageFolder(root=train_dir,
-                                          transform=data_transform,
-                                          target_transform=None)
+training_data = datasets.EMNIST(root='./data/letter_classification/',
+                                            split='letters',
+                                            train=True, 
+                                            download=True, 
+                                            transform=data_transform)
 
-#testing_data = datasets.ImageFolder(root=test_dir,
-#                                    transform=data_transform)
-#Not made yet :)
+testing_data = datasets.EMNIST(root='./data/letter_classification/',
+                                            split='letters',
+                                            train=False, 
+                                            download=True, 
+                                            transform=data_transform)
 
 training_dataloader = DataLoader(dataset=training_data,
                                  batch_size=1,
                                  num_workers=0,
-                                 shuffle=True)
+                                 shuffle=False)
+
+testing_dataloader = DataLoader(dataset=testing_data,
+                                batch_size=1,
+                                shuffle=False,
+                                num_workers=0)
 
 letter_classes = ('a','b','c','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 
 learning_rate = 1e-3
+
+inputs, labels = training_data[0], training_data[1]
+
+print(f'Input: {inputs}, Label: {labels}')
 
 if train:
     criterion = nn.CrossEntropyLoss()
@@ -86,8 +99,8 @@ if train:
             optimiser.step()
 
             running_loss += loss.item()
-            if i% 250 == 249: #Print every 250 mini batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 250:.3f}')
+            if i% 2500 == 2499: #Print every 2500 mini batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2500:.3f}')
                 running_loss = 0.0
     print('Finished training!')
 
